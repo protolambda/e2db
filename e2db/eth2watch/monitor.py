21c7c9d65b1e90cfa5bb932d7b876267b23e5fbb
protolambda <proto@protolambda.com>
2020-06-07 03:04:56 +0200

    Use eth2fastspec and milagro

--- e2db/eth2watch/monitor.py
+++ e2db/eth2watch/monitor.py
@@ -1,20 +1,29 @@
 import trio
-from typing import Dict, Set, Optional
+from typing import Dict, Set, Optional, NamedTuple
 from eth2.models.lighthouse import Eth2API, ForkchoiceData, HeadInfo
 from eth2spec.phase0 import spec
+import eth2fastspec
 from lru import LRU
 import traceback
 
 
-def unchecked_state_transition(state: spec.BeaconState, signed_block: spec.SignedBeaconBlock) -> spec.BeaconState:
+def unchecked_state_transition(epc: eth2fastspec.EpochsContext, state: spec.BeaconState, signed_block: spec.SignedBeaconBlock) -> spec.BeaconState:
     block = signed_block.message
     # Process slots (including those with no blocks) since block
     if block.slot > state.slot:
-        spec.process_slots(state, block.slot)
+        eth2fastspec.process_slots(epc, state, block.slot)
     # Process block
-    spec.process_block(state, block)
+    eth2fastspec.process_block(epc, state, block)
     # Return post-state
     return state
+
+
+class CachedState(NamedTuple):
+    state: spec.BeaconState
+    epc: eth2fastspec.EpochsContext
+
+    def copy(self) -> "CachedState":
+        return CachedState(self.state.copy(), self.epc.copy())
 
 
 class Eth2Monitor(object):
@@ -39,7 +48,7 @@ class Eth2Monitor(object):
         print(f"block getter out: {out.message.hash_tree_root().hex()} key: ({block_root.hex()})")
         return out.copy()
 
-    async def get_state_by_state_root(self, state_root: spec.Root) -> spec.BeaconState:
+    async def get_state_by_state_root(self, state_root: spec.Root) -> CachedState:
         if state_root not in self.state_by_state_root_cache_dict:
             print(f"fetching state by state root {state_root.hex()}")
             api_state = await self.api.beacon.state(root=state_root)
@@ -50,15 +59,16 @@ class Eth2Monitor(object):
                 temp_header.state_root = state.hash_tree_root()
             block_root = spec.Root(temp_header.hash_tree_root())
             print(f"last block of retrieved state: {block_root.hex()}")
+            epc = eth2fastspec.EpochsContext()
+            epc.load_state(state)
+            await self.cache_state(block_root, state, epc)
+            return CachedState(state, epc)
 
-            await self.cache_state(block_root, state)
-            return state.copy()
-
-        out = self.state_by_state_root_cache_dict[state_root]
-        print(f"state (by state root) getter out: {out.hash_tree_root().hex()} key: ({state_root.hex()})")
+        out: CachedState = self.state_by_state_root_cache_dict[state_root]
+        print(f"state (by state root) getter out: {out.state.hash_tree_root().hex()} key: ({state_root.hex()})")
         return out.copy()
 
-    async def get_state_by_block_and_slot(self, block_root: spec.Root, slot: spec.Slot) -> spec.BeaconState:
+    async def get_state_by_block_and_slot(self, block_root: spec.Root, slot: spec.Slot) -> CachedState:
         """May fail if the state is being fetched from API"""
 
         key = (block_root, slot)
@@ -68,15 +78,15 @@ class Eth2Monitor(object):
             signed_block = await self.get_block(block_root)
             pre_state_root = signed_block.message.state_root
             print(f"fetching state {pre_state_root.hex()} (from block: {signed_block.message.hash_tree_root().hex()})")
-            state = await self.get_state_by_state_root(state_root=pre_state_root)
-            if state.slot < slot:
-                spec.process_slots(state, slot)
-            print(f"computed state {state.hash_tree_root().hex()}")
-            await self.cache_state(block_root, state)
-            return state.copy()
+            cached_state = await self.get_state_by_state_root(state_root=pre_state_root)
+            if cached_state.state.slot < slot:
+                eth2fastspec.process_slots(cached_state.epc, cached_state.state, slot)
+            print(f"computed state {cached_state.state.hash_tree_root().hex()}")
+            await self.cache_state(block_root, cached_state.state, cached_state.epc)
+            return cached_state
 
-        out = self.state_by_block_slot_cache_dict[key]
-        print(f"state (by block root and slot) getter out: {out.hash_tree_root().hex()} key: ({block_root.hex()}, {slot})")
+        out: CachedState = self.state_by_block_slot_cache_dict[key]
+        print(f"state (by block root and slot) getter out: {out.state.hash_tree_root().hex()} key: ({block_root.hex()}, {slot})")
         return out.copy()
 
     async def cache_signed_block(self, signed_block: spec.SignedBeaconBlock):
@@ -86,10 +96,10 @@ class Eth2Monitor(object):
         cached = signed_block.copy()
         self.block_cache[block_root] = cached
 
-    async def cache_state(self, block_root: spec.Root, state: spec.BeaconState):
+    async def cache_state(self, block_root: spec.Root, state: spec.BeaconState, epc: eth2fastspec.EpochsContext):
         print(f"caching state (last block: {block_root.hex()}, slot: {state.latest_block_header.slot}) "
               f"{state.hash_tree_root().hex()} (state slot {state.slot})")
-        cached = state.copy()
+        cached = CachedState(state.copy(), epc.copy())
         self.state_by_block_slot_cache_dict[(block_root, state.slot)] = cached
         self.state_by_state_root_cache_dict[state.hash_tree_root()] = cached
 
@@ -97,20 +107,22 @@ class Eth2Monitor(object):
         block = signed_block.message
         block_root = spec.Root(block.hash_tree_root())
         prev_slot = spec.Slot(block.slot - 1)
-        pre_state: spec.BeaconState
+        pre_state_cached: CachedState
         try:
-            pre_state = await self.get_state_by_block_and_slot(block.parent_root, prev_slot)
+            pre_state_cached = await self.get_state_by_block_and_slot(block.parent_root, prev_slot)
         except Exception as e:
             print(f"Failed to fetch state: {e}")
             return False
-        state = pre_state.copy()
+        state = pre_state_cached.state
+        pre_state = state.copy()
+        epc = pre_state_cached.epc
 
         print(f"processing state transition of block {block_root} (slot {block.slot}) "
               f"on state {state.hash_tree_root().hex()} (slot {state.slot}) "
               f"(asked for state of parent {block.parent_root.hex()}, at slot {prev_slot})")
 
         try:
-            unchecked_state_transition(state, signed_block)
+            unchecked_state_transition(epc, state, signed_block)
         except Exception as e:
             print(f"WARNING: {block_root.hex()} (slot {block.slot}) failed state transition: {e}")
             traceback.print_exc()
@@ -119,12 +131,14 @@ class Eth2Monitor(object):
             print(f"WARNING: {block.hash_tree_root().hex()} (slot {block.slot}) state root ({block.state_root.hex()})"
                   f" does not match computed state root ({state.hash_tree_root().hex()})")
             return False
-        await self.cache_state(block_root, state)
+        await self.cache_state(block_root, state, epc)
         await dest.send((pre_state, state.copy(), signed_block, is_canon))
         return True
 
-    async def _fetch_state_empty_slots(self, state: spec.BeaconState, delta_slots: int, dest: trio.MemorySendChannel, deltas_canon: int) -> spec.BeaconState:
+    async def _fetch_state_empty_slots(self, epc: eth2fastspec.EpochsContext, state: spec.BeaconState,
+                                       delta_slots: int, dest: trio.MemorySendChannel, deltas_canon: int) -> spec.BeaconState:
         state = state.copy()
+        epc = epc.copy()
         start_slot = state.slot + 1
         to_slot = start_slot + delta_slots
         # If deltas_canon == 0, then none of the slots are canon
@@ -132,9 +146,9 @@ class Eth2Monitor(object):
         for slot in range(start_slot, to_slot):
             pre_state = state.copy()
             print(f"processing state transition of empty slot {slot} on pre-state {pre_state.hash_tree_root().hex()})")
-            spec.process_slots(state, spec.Slot(slot))
+            eth2fastspec.process_slots(epc, state, spec.Slot(slot))
             block_root = spec.Root(state.latest_block_header.hash_tree_root())
-            await self.cache_state(block_root, state)
+            await self.cache_state(block_root, state, epc)
             is_canon = slot < canon_to_slot
             await dest.send((pre_state, state.copy(), None, is_canon))
         return state
@@ -222,9 +236,9 @@ class Eth2Monitor(object):
 
                 # If the node is a start of a gap to a later unprocessed node, then process the empty slots
                 if node.root in empty_slots:
-                    pre_state: spec.BeaconState
+                    pre_state_cached: CachedState
                     try:
-                        pre_state = await self.get_state_by_block_and_slot(node.root, node.slot)
+                        pre_state_cached = await self.get_state_by_block_and_slot(node.root, node.slot)
                     except Exception as e:
                         print(f"Failed to fetch state: {e}")
                         continue
@@ -234,7 +248,8 @@ class Eth2Monitor(object):
                     # Count them, and share that these will be canonical.
                     following_canon_empty_slots = 0 if node.root not in canon_empty_slots else len(canon_empty_slots[node.root])
                     print(f"after {node.root} {following_empty_slots} empty slots follow")
-                    await self._fetch_state_empty_slots(state=pre_state,
+                    await self._fetch_state_empty_slots(epc=pre_state_cached.epc,
+                                                        state=pre_state_cached.state,
                                                         delta_slots=following_empty_slots, dest=dest,
                                                         deltas_canon=following_canon_empty_slots)
 
@@ -256,7 +271,9 @@ class Eth2Monitor(object):
         api_block = await self.api.beacon.block(slot=spec.Slot(from_slot-1))
         if genesis:
             api_state = await self.api.beacon.state(slot=spec.Slot(0))
-            await self.cache_state(api_block.root, api_state.beacon_state)
+            epc = eth2fastspec.EpochsContext()
+            epc.load_state(api_state.beacon_state)
+            await self.cache_state(api_block.root, api_state.beacon_state, epc)
             await dest.send((None, api_state.beacon_state, None, True))
         prev_block_root = api_block.root
         print(f"starting block root: {prev_block_root}")
@@ -273,12 +290,12 @@ class Eth2Monitor(object):
                 print(f"empty slot {slot}")
                 # We got the same block again, it's an empty slot.
                 try:
-                    pre_state = await self.get_state_by_block_and_slot(prev_block_root, spec.Slot(slot-1))
+                    pre_state_cached = await self.get_state_by_block_and_slot(prev_block_root, spec.Slot(slot-1))
                 except Exception as e:
                     print(f"Failed to fetch state for slot {slot} after block {prev_block_root.hex()}: {e}")
                     await trio.sleep(step_slowdown)
                     continue
-                await self._fetch_state_empty_slots(pre_state, 1, dest, 1)
+                await self._fetch_state_empty_slots(pre_state_cached.epc, pre_state_cached.state, 1, dest, 1)
                 print(f"completed processing empty slot {slot}")
             else:
                 print(f"block {signed_block.message.hash_tree_root().hex()} state_root: {signed_block.message.state_root.hex()} parent_root: {signed_block.message.parent_root.hex()} slot: {signed_block.message.slot}")
